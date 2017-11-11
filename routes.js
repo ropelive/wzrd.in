952ef003e1b473408301c5ea56f5fa8c9f85b655
Gokmen Goksel <gokmen@goksel.me>
2017-11-10 21:56:32 -0800

    Routes: simplify routes and disable purge routes

--- routes.js
+++ routes.js
@@ -14,21 +14,8 @@ module.exports = function (app, bundler, config) {
   app.get('/standalone/:slug', singularHandler(bundler, { standalone: true }));
   app.get('/debug-standalone/:slug', singularHandler(bundler, { standalone: true, debug: true }));
 
-  app.post('/multi', bodyParser.json(), multiHandlers.create(bundler));
+  app.post('/multi', bodyParser.json({type:'*/*'}), multiHandlers.create(bundler));
+
   app.get('/multi/:bundle', multiHandlers.get(bundler));
-
   app.get('/status/:bundle', statusesHandler(bundler));
-
-  // Purge routes.
-  // The PURGE method routes are dead code, For Now, since the "admin"
-  // interface is already being used by the mods, with proposed work to
-  // migrate to PURGE methods in future work.
-  app.get('/admin/cull/:module', auth(config), cullHandler(bundler, config));
-  /*
-  app.purge('/bundle/:slug', singularHandler(bundler, { purge: true }));
-  app.purge('/debug-bundle/:slug', singularHandler(bundler, { debug: true, purge: true }));
-  app.purge('/standalone/:slug', singularHandler(bundler, { standalone: true, purge: true }));
-  app.purge('/debug-standalone/:slug', singularHandler(bundler, { standalone: true, debug: true, purge: true }));
-  app.purge('/multi/:bundle', multiHandlers.purge(bundler));
-  */
 }
